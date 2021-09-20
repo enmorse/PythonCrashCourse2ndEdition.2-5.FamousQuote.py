3c164462e7b721fb9e044408bd5861f05969b67a
Morse <enmorse48@gmail.com>
2021-09-19 21:43:52 -0700

    Initial commit

---
+++ Main.py
@@ -0,0 +1,9 @@
+# Find a quote from a famous person you admire,
+# and print the quote and the name of the author.
+
+name = "socrates"
+quote = "'Strong minds discuss ideas" \
+        "\nAverage minds discuss events" \
+        "\nWeak minds discuss people'"
+message = f"{name.title()} once said, {quote}"
+print(message)
